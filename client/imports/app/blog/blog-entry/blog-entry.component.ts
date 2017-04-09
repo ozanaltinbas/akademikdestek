658dcf2137f09bebfc2e1d829b44fb1eb4bcbb50
ozanaltinbas <ozan.altinbas@ymail.com>
2017-04-09 04:40:24 +0300

    hop.

--- client/imports/app/blog/blog-entry/blog-entry.component.ts
+++ client/imports/app/blog/blog-entry/blog-entry.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MeteorObservable } from 'meteor-rxjs';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { InjectUser } from "angular2-meteor-accounts-ui";
@@ -14,7 +14,7 @@ import style from './blog-entry.component.scss';
     styles: [ style ]
 })
 @InjectUser('user')
-export class BlogEntryComponent implements OnInit {
+export class BlogEntryComponent implements OnInit, OnDestroy {
 
     blogForm: FormGroup;
     user: Meteor.User;
@@ -26,6 +26,7 @@ export class BlogEntryComponent implements OnInit {
 
     ngOnInit() {
         this.initializeBlogEntryForm();
+        this.initEditor();
     }
 
     ngAfterViewInit(): void {
@@ -36,20 +37,21 @@ export class BlogEntryComponent implements OnInit {
         // initialize blog form
         this.blogForm = this.formBuilder.group({
             title: ['', Validators.required],
-            subtitle: ['', Validators.required],
-            content: ['', Validators.required]
+            subtitle: ['', Validators.required]
         });
     }
 
     insertBlog(event) {
         // prevent default event.
         event.preventDefault();
-        // if the mail form is valid,
-        if (this.blogForm.valid) {
+        // get the value.
+        let content = $('div#froala-editor').froalaEditor('html.get');
+        // if the blog form is valid,
+        if (this.blogForm.valid && content && content.length > 0) {
             // set as on progress
             this.onProgress = true;
             // send the mail.
-            MeteorObservable.call('insertBlog', this.blogForm.value.title, this.blogForm.value.subtitle, this.blogForm.value.content, this.user._id).subscribe(() => {
+            MeteorObservable.call('insertBlog', this.blogForm.value.title, this.blogForm.value.subtitle, content, this.user._id).subscribe(() => {
                 // mail successfully sent.
                 this.initializeBlogEntryForm();
                 this.onProgress = false;
@@ -57,6 +59,36 @@ export class BlogEntryComponent implements OnInit {
                 this.onProgress = false;
             });
         }
+    }
+
+    initEditor() {
+        $(function() {
+            $.FroalaEditor.RegisterCommand('clear', {
+                title: 'Clear HTML',
+                focus: false,
+                undo: true,
+                refreshAfterCallback: true,
+                callback: function () {
+                    this.html.set('');
+                    this.events.focus();
+                }
+            });
+            $('div#froala-editor').froalaEditor({
+                toolbarButtons: ['fontFamily', '|' , 'fontSize', '|', 'align', '|', 'paragraphFormat', '|' , 'formatOL', '|' , 'formatUL', '|', 'bold', '|', 'italic', '|', 'underline', '|', 'undo', '|', 'redo', '|', 'outdent', '|', 'indent', '|', 'clearFormatting', '|', 'insertTable', '|', 'html'],
+                heightMin: 200,
+                heightMax: 500
+            })
+        });
+    }
+
+    destroyEditor() : void {
+        if ($('div#froala-editor').data('froala.editor')) {
+            $('div#froala-editor').froalaEditor('destroy');
+        }
+    }
+
+    ngOnDestroy() {
+        this.destroyEditor();
     }
 
 }
--- client/imports/app/posts/posts.component.ts
+++ client/imports/app/posts/posts.component.ts
@@ -15,7 +15,7 @@ export class PostsComponent {
     }
 
     ngAfterViewInit(): void {
-
+        
     }
 
 }
