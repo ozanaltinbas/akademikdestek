bc10c58ff5c416e3a14afeae4ab021b04cb7b38d
ozanaltinbas <ozan.altinbas@ymail.com>
2017-05-14 21:25:02 +0300

    hop.

--- client/imports/pipes/display-profile-image.pipe.ts
+++ client/imports/pipes/display-profile-image.pipe.ts
@@ -7,12 +7,15 @@ import { Images } from '../../../both/collections/images.collection';
 export class DisplayProfileImagePipe implements PipeTransform {
     
     transform(imageId: string) {
+        console.log(imageId);
         // set default imageUrl
         let imageUrl: string = 'img/icons/account_circle.png';
         // if there is an incoming imageId
         if (imageId && imageId.length > 0) {
+            console.log("imageId sent");
             // yeah, there is an incoming imageId. get the image object
             const found = Images.findOne(imageId);
+            console.log(found);
             // if there is a image with it
             if (found) {
                 // assign its url to returned value
