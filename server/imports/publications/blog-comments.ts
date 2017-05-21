eb5fc69c944e1dd9c257167ebb6f203ded1e2176
ozanaltinbas <ozan.altinbas@ymail.com>
2017-05-21 23:37:30 +0300

    hop.

--- server/imports/publications/blog-comments.ts
+++ server/imports/publications/blog-comments.ts
@@ -4,7 +4,20 @@ import { Options } from '../../../both/models/options.model';
 
 import { BlogComments } from '../../../both/collections/blog-comments.collection';
 
-Meteor.publish('blog-comments', function(blogId: string, options: Options) {
-    Counts.publish(this, 'numberOfBlogComments', BlogComments.collection.find({ blogId: blogId, public: true }), { noReady: true });
-    return BlogComments.find({ blogId: blogId, public: true }, options);
+Meteor.publishComposite('blog-comments', function(blogId: string, options: Options) {
+    return {
+        find() {
+            Counts.publish(this, 'numberOfBlogComments', BlogComments.collection.find({ blogId: blogId, public: true }), { noReady: true });
+            return BlogComments.find({ blogId: blogId, public: true }, options);
+        },
+        children: [
+            {
+                find(blogComment) {
+                    const options = { fields: { username: 1 }};
+                    return Meteor.users.find({ _id : blogComment.owner }, options);
+                }
+            }
+        ]
+    };
+
 });
--- server/imports/publications/post-comments.ts
+++ server/imports/publications/post-comments.ts
@@ -4,7 +4,20 @@ import { Options } from '../../../both/models/options.model';
 
 import { PostComments } from '../../../both/collections/post-comments.collection';
 
-Meteor.publish('post-comments', function(postId: string, options: Options) {
-    Counts.publish(this, 'numberOfPostComments', PostComments.collection.find({ postId: postId, public: true }), { noReady: true });
-    return PostComments.find({ postId: postId, public: true }, options);
+Meteor.publishComposite('post-comments', function(postId: string, options: Options) {
+    return {
+        find() {
+            Counts.publish(this, 'numberOfPostComments', PostComments.collection.find({ postId: postId, public: true }), { noReady: true });
+            return PostComments.find({ postId: postId, public: true }, options);
+        },
+        children: [
+            {
+                find(postComment) {
+                    const options = { fields: { username: 1 }};
+                    return Meteor.users.find({ _id : postComment.owner }, options);
+                }
+            }
+        ]
+    };
+
 });
